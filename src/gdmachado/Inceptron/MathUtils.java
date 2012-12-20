7e4bf9f7d9aec35fc06673c760e94db0c4f67995
Gustavo Machado <gdmachado@me.com>
2012-12-20 22:45:57 +0000

    Corrected dsigmoid method, added getDelta method

--- src/gdmachado/Inceptron/MathUtils.java
+++ src/gdmachado/Inceptron/MathUtils.java
@@ -1,6 +1,11 @@
 package gdmachado.Inceptron;
 
 public class MathUtils {
+	public static double getDelta(double output, double expected, double lambda)
+	{
+		return dsigmoid(output, lambda) * (expected - output);
+	}
+	
 	public static Double dotProduct(final double[] a, final double[] b)
 	{		
 		double result = 0;
@@ -9,7 +14,7 @@ public class MathUtils {
 		}
 		catch (ArrayIndexOutOfBoundsException e)
 		{
-			System.err.println("Number of inputs does not match the number of weights");
+			System.err.println("Number of inputs does not match the number of weights. Exiting...");
 			System.exit(1);
 		}
 		return result;
@@ -27,7 +32,7 @@ public class MathUtils {
 	
 	public static double dsigmoid(double x, double lambda)
 	{
-		return lambda * x * (1 - x);
+		return lambda * (Math.pow(Math.E, (-1) * lambda * x) / (Math.pow(Math.pow(Math.E, (-1) * lambda * x), 2)));
 	}
 	
 	public static double tanh(double x, double lambda)
@@ -37,6 +42,28 @@ public class MathUtils {
 	
 	public static double rootMeanSquare(double[] values)
 	{
-		return values[0];
+		double sum = 0;
+		for(double n : values)
+			sum += Math.pow(n, 2);
+		sum /= values.length;
+		return sum;
+		
+	}
+	
+	public static double rootMeanSquareError(double[] output, double[] expected)
+	{
+		double sum = 0;
+		try
+		{
+			for(int i = 0; i < output.length; i++)
+				sum += Math.pow(output[i] - expected[i], 2);
+			sum = Math.sqrt(sum / output.length);
+		}
+		catch (ArrayIndexOutOfBoundsException e)
+		{
+			System.err.println("Number of outputs does not match number of expected values. Exiting...");
+			System.exit(1);
+		}
+		return sum;
 	}
 }
