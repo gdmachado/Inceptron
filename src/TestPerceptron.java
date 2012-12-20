e48cb50f28ba11a4aff04f5b5ff940230dcb9803
Gustavo Machado <gdmachado@me.com>
2012-12-20 18:42:35 +0000

    Adjustments

--- src/TestPerceptron.java
+++ src/TestPerceptron.java
@@ -3,21 +3,30 @@ import gdmachado.Inceptron.*;
 public class TestPerceptron {
 
 	public static void main(String[] args) {
-		double[][] data = { {1,0,0}, {1,0,1}, {1,1,0}, {1,1,1} };
-		double[] expectedOutput = {1,1,1,0};
+		double[][] data = { {0,0}, {0,1}, {1,0}, {1,1} };
+		double[] expectedOutput = {1,0,0,0};
 		
-		Perceptron p = new Perceptron(3, 0, 0.1, 0.5, 0, 1, true);
+		Neuron p = new Neuron(2, 0.5, 0.3, 0.05, -1, 1);
 		
-		p.train(data, expectedOutput);
+		System.out.println("Beginning training...");
+		
+		int it = Teacher.train(data, expectedOutput, 0.001d, p, true);
+		
+		System.out.println("Finished training in "+it+" iterations.");
 		
 		System.out.println("\nStarting test");
 		
 		double[] output = p.test(data);
 		
 		for (int i = 0; i < data.length; i++)
-			System.out.println("Output for {"+data[i][0]+", "+data[i][1]+", "+data[i][2]+"}: "
-												+output[i]+"; Expected: "+expectedOutput[i]);
-
+		{
+			System.out.print("Output for {");
+			for (int j = 0; j < data[i].length - 1; j++)
+				System.out.print(data[i][j]+", ");
+			System.out.print(data[i][data[i].length-1]+"}: " + output[i]);
+			System.out.println(", Expected: "+expectedOutput[i]);
+		}
+		
         for (double n : p.getWeights())
         	System.out.print(n+", ");
 	}
